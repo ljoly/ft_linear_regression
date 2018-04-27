105c13d70ce1c6bf8b0d03ebb2ff95f6f71e3687
Louis-marie JOLY <ljoly@e2r11p1.42.fr>
2018-04-27 12:37:04 +0200

    const

--- cmd/training/linearRegression.go
+++ cmd/training/linearRegression.go
@@ -5,6 +5,12 @@ var (
 	theta1 float64
 	sum0   float64
 	sum1   float64
+	length float64
+)
+
+const (
+	learningRate = 0.3
+	iterations   = 1000
 )
 
 func getEstimatePrice(mileage float64) float64 {
@@ -12,20 +18,19 @@ func getEstimatePrice(mileage float64) float64 {
 }
 
 func linearRegression() {
-	learningRate := 0.3
-	len := float64(len(carsNormalized))
+	length = float64(len(carsNormalized))
 
 	theta0 = 1
 	theta1 = 1
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < iterations; i++ {
 		sum0 = 0
 		sum1 = 0
 		for _, v := range carsNormalized {
 			sum0 += getEstimatePrice(v.mileage) - v.price
 			sum1 += (getEstimatePrice(v.mileage) - v.price) * v.mileage
 		}
-		theta0 -= learningRate / len * sum0
-		theta1 -= learningRate / len * sum1
+		theta0 -= learningRate / length * sum0
+		theta1 -= learningRate / length * sum1
 	}
 	denormalizeThetas()
 }
